b9838bdedd10151cba0763f5b3cca907e0040a41
Vladimir <w112vovan@mail.ru>
2023-11-29 13:22:23 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L22_localMaxima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] mainArray = new int[30];
+            int minNumber = 0;
+            int maxNumber = 100;
+
+            Console.WriteLine("Массив:");
+
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                mainArray[i] = random.Next(minNumber, maxNumber + 1);
+                Console.Write(mainArray[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("\nЛокальные максимумы:");
+
+            for (int i = 0; i < mainArray.Length; i++)
+            {
+                if (i > 0 && i < mainArray.Length - 1)
+                {
+                    if (mainArray[i] > mainArray[i - 1] && mainArray[i] > mainArray[i + 1])
+                        Console.Write(mainArray[i] + " ");
+                }
+                else if (i == 0)
+                {
+                    if (mainArray[i] > mainArray[i + 1])
+                        Console.Write(mainArray[i] + " ");
+                }
+                else if (i == mainArray.Length - 1)
+                {
+                    if (mainArray[i] > mainArray[i - 1])
+                        Console.Write(mainArray[i] + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("L22_localMaxima")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L22_localMaxima")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b0db322e-4145-4e9c-8e35-1bbe06ca9031")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
